f2f923061f00e45731adcb83f9de629949075f0c
Nicholas Sharp <naas8905@gmail.com>
2020-06-16 18:10:25 -0700

    Adding more nav styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,11 +5,11 @@ export default class Home extends Component {
     return (
       <div>
         <h2>HELLO</h2>
-        <p>Cras facilisis urna ornare ex volutpat, et
-        convallis erat elementum. Ut aliquam, ipsum vitae
-        gravida suscipit, metus dui bibendum est, eget rhoncus nibh
-        metus nec massa. Maecenas hendrerit laoreet augue
-        nec molestie. Cum sociis natoque penatibus et magnis
+        <p>Cras facilisis urna ornare ex volutpat, et<br></br>
+        convallis erat elementum. Ut aliquam, ipsum vitae<br></br>
+        gravida suscipit, metus dui bibendum est, eget rhoncus nibh<br></br>
+        metus nec massa. Maecenas hendrerit laoreet augue<br></br>
+        nec molestie. Cum sociis natoque penatibus et magnis<br></br>
         dis parturient montes, nascetur ridiculus mus.</p>
  
         <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
