3103ae8051df6b3f395f3dbcbf2cfb1137eabea0
Nicholas Sharp <naas8905@gmail.com>
2020-06-16 17:59:06 -0700

    Adding styles

--- src/components/Main.js
+++ src/components/Main.js
@@ -21,7 +21,7 @@ export default class Main extends Component {
             <li><NavLink to="/contact">Contact</NavLink></li>
           </ul>
           <div className="content">
-            <Route path="/" component={Home}/>
+            <Route exact path="/" component={Home}/>
             <Route path="/stuff" component={Stuff}/>
             <Route path="/contact" component={Contact}/>
           </div>
--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Main from "./Main"
+import "./styles.css";
 
 export default class App extends Component {
   render() {
