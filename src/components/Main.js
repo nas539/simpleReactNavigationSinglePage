fa5d22ae2d339cf3bbe2fa3330fc2d14244e93d9
Nicholas Sharp <naas8905@gmail.com>
2020-06-17 08:07:38 -0700

    Fixing the Home button

--- src/components/Main.js
+++ src/components/Main.js
@@ -14,9 +14,9 @@ export default class Main extends Component {
     return (
       <HashRouter>
         <div>
-          <h1>Simple SPA</h1>
+          
           <ul className="header">
-            <li><NavLink to="/">Home</NavLink></li>
+            <li><NavLink exact to="/">Home</NavLink></li>
             <li><NavLink to="/stuff">Stuff</NavLink></li>
             <li><NavLink to="/contact">Contact</NavLink></li>
           </ul>
